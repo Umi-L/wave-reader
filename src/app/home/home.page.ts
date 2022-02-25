7efcb041f17a6726a6e1ec2503ebdaef8d6c06f8
umi <julianogrady07@gmail.com>
2022-02-25 15:23:18 -0500

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+declare var ePub: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
   constructor() {}
 
+  ngOnInit(){
+
+    const bookInput = document.getElementById("bookInput");
+    bookInput.onchange = function(e){
+      let book = ePub((<HTMLInputElement>bookInput).files[0]);
+
+      var rendition = book.renderTo("area", { method: "continuous", width: "100%", height: "100%", allowScriptedContent: true});
+      rendition.themes.default("../assets/fontSheet.css")
+      var displayed = rendition.display();
+    }
+  }
 }
